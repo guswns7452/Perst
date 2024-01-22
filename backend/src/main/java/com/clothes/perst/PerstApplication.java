5069094d0afb84517e9714bc919301bb0bd815fe
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-01-22 20:34:13 +0900

    :wrench: build: 기타 파일들 업로드

---
+++ backend/src/main/java/com/clothes/perst/PerstApplication.java
@@ -0,0 +1,13 @@
+package com.clothes.perst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerstApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/clothes/perst/PerstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothes.perst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
