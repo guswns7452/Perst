afd1f1fa7cd0a55c99c02d9175769975f39da9b2
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-01-27 20:45:32 +0900

    :recycle: refactor: MemberRepository DI injection

--- backend/src/main/java/com/clothes/perst/persistance/MemberRepository.java
+++ backend/src/main/java/com/clothes/perst/persistance/MemberRepository.java
@@ -2,7 +2,9 @@ package com.clothes.perst.persistance;
 
 import com.clothes.perst.domain.MemberVO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<MemberVO, Integer> {
     // 특별한 쿼리 메서드가 필요 없는 경우, JpaRepository에서 제공하는 메서드들로 데이터를 조회할 수 있습니다.
     MemberVO findByMemberPhoneAndMemberPassword(String memberPhone, String memberPassword);
