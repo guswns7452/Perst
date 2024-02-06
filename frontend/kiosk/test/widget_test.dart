61adc1ff5af80c6d196718c4371e7f0026553bf0
dahyeon-da <pop8261@naver.com>
2024-02-06 20:29:26 +0900

    🐛fix: 설정파일 오류 수정

--- frontend/kiosk/test/widget_test.dart
+++ frontend/kiosk/test/widget_test.dart
@@ -7,8 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:kiosk/main.dart';
 import 'package:kiosk/src/app.dart';
 
 void main() {
