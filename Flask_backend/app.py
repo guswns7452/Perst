88dbfec5b308bc3075349f83a735bcc0db53e517
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-02-07 03:41:54 +0900

    :sparkles: feat: Flask 구글 드라이브 다운로드 코드 반영 (미완성 : OAuth 반영)
    OAuth2 반영이 시간이 걸릴 것 같아, 보류

--- Flask_backend/app.py
+++ Flask_backend/app.py
@@ -10,6 +10,10 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+import io
+import google.auth
+from googleapiclient.http import MediaIoBaseDownload
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/drive.metadata.readonly"]
 
@@ -54,11 +58,50 @@ def googleDrive(filename):
             raise FileNotFoundError("일치하는 파일이 없습니다.")
         print("Files:")
         for item in items:
+            DownloadByGoogleDrive(file_id=item['id'])
             print(f"{item['name']} ({item['id']})")
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
         raise ConnectionError("구글 드라이브 API 문제입니다.")
     return items
+
+def DownloadByGoogleDrive(file_id):
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+    # create drive api client
+        service = build("drive", "v3", credentials=creds)
+
+        # pylint: disable=maybe-no-member
+        request = service.files().get_media(fileId=file_id)
+        file = io.BytesIO()
+        downloader = MediaIoBaseDownload(file, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print(f"Download {int(status.progress() * 100)}.")
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        file = None
+
+    return file.getvalue()
+
 
 @app.route('/style/analyze', methods=['POST'])
 def analyzeAPI():
