5e726147ab371302ee2195cfb9d492dc4b94152c
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-02-27 01:08:53 +0900

    :sparkles: feat: Musinsa Data Find Rest API 구축

--- backend/src/main/java/com/clothes/perst/persistance/MusinsaRepository.java
+++ backend/src/main/java/com/clothes/perst/persistance/MusinsaRepository.java
@@ -3,6 +3,9 @@ package com.clothes.perst.persistance;
 import com.clothes.perst.domain.MusinsaVO;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MusinsaRepository extends CrudRepository<MusinsaVO, MusinsaVO> {
     MusinsaVO save(MusinsaVO musinsaVO);
+    List<MusinsaVO> findAllByMusinsaGenderAndMusinsaStyle(String musinsaGender, String musinsaStyle);
 }
