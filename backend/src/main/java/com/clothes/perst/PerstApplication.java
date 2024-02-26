912283e82d205992bd78e00d3fcc50b6dad7e40f
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-02-27 02:51:28 +0900

    :card_file_box: feat: 생성시간 자동화

--- backend/src/main/java/com/clothes/perst/PerstApplication.java
+++ backend/src/main/java/com/clothes/perst/PerstApplication.java
@@ -2,8 +2,10 @@ package com.clothes.perst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PerstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PerstApplication.class, args);
--- backend/src/main/java/com/clothes/perst/domain/StyleAnalyzeVO.java
+++ backend/src/main/java/com/clothes/perst/domain/StyleAnalyzeVO.java
@@ -3,12 +3,15 @@ package com.clothes.perst.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "style_analyze") // 테이블 이름을 명시해줄 수 있습니다.
 public class StyleAnalyzeVO {
     @Id
@@ -25,6 +28,6 @@ public class StyleAnalyzeVO {
 
     private String styleColor;
 
-    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
     private Date styleDate;
 }
