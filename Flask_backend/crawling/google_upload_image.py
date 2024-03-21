8b6ed9436937843482f46798518466b26f8c98de
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-03-22 02:48:53 +0900

    :sparkles: feat: 구글 드라이브에서 1000개 이상의 이미지를 검색할 때 사용

--- Flask_backend/crawling/google_upload_image.py
+++ Flask_backend/crawling/google_upload_image.py
@@ -10,6 +10,9 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
 
+# token 불러오기가 잘 되지 않아 PATH 설정
+PATH =  os.getcwd()
+
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 def upload_basic(file_name, img_path, gender, index, height, weight, season, style):
@@ -21,17 +24,17 @@ def upload_basic(file_name, img_path, gender, index, height, weight, season, sty
     for guides on implementing OAuth2 for the application.
     """
     
-    if os.path.exists("../token.json"):
-        creds = Credentials.from_authorized_user_file("../token.json", SCOPES)
+    if os.path.exists(PATH+"/token.json"):
+        creds = Credentials.from_authorized_user_file(PATH+"/token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("../credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(PATH+"/credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("../token.json", "w") as token:
+        with open(PATH+"/token.json", "w") as token:
             token.write(creds.to_json())
             
     try:
@@ -61,17 +64,17 @@ def googleDrive():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("../token.json"):
-        creds = Credentials.from_authorized_user_file("../token.json", SCOPES)
+    if os.path.exists(PATH+"/token.json"):
+        creds = Credentials.from_authorized_user_file(PATH+"/token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("../credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(PATH+"/credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open("../stoken.json", "w") as token:
+        with open(PATH+"/token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
@@ -80,16 +83,30 @@ def googleDrive():
         # Call the Drive v3 API
         results = (
             service.files()
-            .list(q="name contains '_걸리시'" ,pageSize=10, fields="nextPageToken, files(id, name)")
+            .list(q="parents in '10neLNimYRBdqqEDqH9-aN8c3KK5gcXa5'", pageSize=1000, fields="nextPageToken, files(id, name)")
             .execute()
         )
-        items = results.get("files", [])
+        
+        ##
+        # 1000개 이상의 데이터가 있는 경우
+        #
+        items = results.get('files', [])
+        next_page_token = results.get('nextPageToken')
+
+        # 다음 페이지가 있는 경우 추가 페이지 요청
+        while next_page_token:
+            results = service.files().list(q="parents in '10neLNimYRBdqqEDqH9-aN8c3KK5gcXa5'", pageSize=1000, pageToken=next_page_token).execute()
+            items.extend(results.get('files', []))
+            next_page_token = results.get('nextPageToken')
 
         if not items:
             raise FileNotFoundError("일치하는 파일이 없습니다.")
         print("Files:")
+        count = 0
+        
         for item in items:
-            print(f"{item['name']} ({item['id']}) ")
+            count += 1
+            print(f"{count}: {item['name']} ({item['id']}) ")
             # DownloadByGoogleDrive(filename=item['name'], file_id=item['id'])
     except HttpError as error:
         # TODO(developer) - Handle errors from drive API.
