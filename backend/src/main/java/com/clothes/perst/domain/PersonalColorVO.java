93cc8f5e85d101e1d019f33740d898ba78c99f42
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-03-27 19:36:13 +0900

    :card_file_box: feat: 퍼스널 컬러 진단에 사용되는 DB 객체 생성

---
+++ backend/src/main/java/com/clothes/perst/domain/PersonalColorVO.java
@@ -0,0 +1,32 @@
+package com.clothes.perst.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "personal_color") // 테이블 이름을 명시해줄 수 있습니다.
+@Schema(name = "personal_color", description = "퍼스널 컬러 진단 객체")
+public class PersonalColorVO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int personalColorNumber;
+
+    private String personalColorType; // 진단한 퍼스널 컬러 타입 / ex : "겨울 딥"
+
+    private int personalColorAllTimes; // 전체 퍼스널 컬러 진단 횟수
+
+    private Date personalColorDate; // 퍼스널 컬러 날짜
+
+    @Transient // DB에 포함하지 않는 변수
+    private List<PersonalSelectVO> personalSelects;
+
+    private int memberNumber; // 외래키
+}
---
+++ backend/src/main/java/com/clothes/perst/domain/PersonalSelectVO.java
@@ -0,0 +1,26 @@
+package com.clothes.perst.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "personal_select") // 테이블 이름을 명시해줄 수 있습니다.
+@Schema(name = "personal_select", description = "퍼스널 컬러 선택 횟수")
+public class PersonalSelectVO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int personalSelectNumber;
+
+    private String personalSelectType;
+
+    private int personalSelectTimes;
+
+    private int personalColorNumber;
+}
