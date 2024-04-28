badee4a4d20b04cc06c0bb137674c806ca087ae4
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-04-28 23:35:25 +0900

    :bug: fix: WAR 파일에서 JSON을 불러오지 못하는 오류 수정

--- backend/src/main/java/com/clothes/perst/DTO/ColorToRGB.java
+++ backend/src/main/java/com/clothes/perst/DTO/ColorToRGB.java
@@ -2,34 +2,38 @@ package com.clothes.perst.DTO;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ColorToRGB {
 
-    public static List<Map<String, int[]>> getHSV(String color){
-        String jsonFilePath = "src/main/resources/colorToHSV.json";
-
-        // ObjectMapper 객체 생성
+    public static List<Map<String, int[]>> getHSV(String color) {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try {
-            // JSON 파일을 읽어서 HashMap에 데이터 로드
-            File jsonFile = new File(jsonFilePath);
+            // ClassPathResource를 통해 InputStream으로 JSON 파일 읽기
+            Resource resource = new ClassPathResource("colorToHSV.json");
+            InputStream inputStream = resource.getInputStream();
+
+            // InputStream을 통해 JSON 파일을 읽고 Map<String, List<Map<String, int[]>>>으로 변환
             TypeReference<HashMap<String, List<Map<String, int[]>>>> typeRef = new TypeReference<HashMap<String, List<Map<String, int[]>>>>() {};
-            HashMap<String, List<Map<String, int[]>>> colorMap = objectMapper.readValue(jsonFile, typeRef);
+            HashMap<String, List<Map<String, int[]>>> colorMap = objectMapper.readValue(inputStream, typeRef);
 
+            // InputStream 사용 후 닫기
+            inputStream.close();
+
+            // 주어진 색상에 대한 HSV 값을 반환
             return colorMap.get(color);
-
         } catch (IOException e) {
             System.err.println("파일 읽기 오류: " + e.getMessage());
         }
 
         return null;
     }
-
 }
