178f17050f8630b6b500b0d8097192725ab63f9b
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-04-28 23:00:54 +0900

    :bug: fix: Request Param이 List로 할당되지 않는 오류 수정
    - 새 객체로 변경

---
+++ backend/src/main/java/com/clothes/perst/DTO/ColorRequest.java
@@ -0,0 +1,12 @@
+package com.clothes.perst.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ColorRequest {
+    private List<String> color;
+}
