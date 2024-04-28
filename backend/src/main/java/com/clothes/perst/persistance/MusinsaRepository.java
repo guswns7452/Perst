a6078a38da9c4b74f8401fa35e1bfb2869e70bdd
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-04-28 22:35:34 +0900

    :recycle: refactor: Add Annotation

--- backend/src/main/java/com/clothes/perst/persistance/MusinsaRepository.java
+++ backend/src/main/java/com/clothes/perst/persistance/MusinsaRepository.java
@@ -1,11 +1,13 @@
 package com.clothes.perst.persistance;
 
 import com.clothes.perst.domain.MusinsaVO;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public interface MusinsaRepository extends CrudRepository<MusinsaVO, MusinsaVO> {
-    MusinsaVO save(MusinsaVO musinsaVO);
+@Repository
+public interface MusinsaRepository extends JpaRepository<MusinsaVO, Long> {
+
     List<MusinsaVO> findAllByMusinsaGenderAndMusinsaStyle(String musinsaGender, String musinsaStyle);
 }
