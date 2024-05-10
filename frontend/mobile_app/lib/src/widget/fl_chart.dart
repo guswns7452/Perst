a13af54160668f5d82edd95f45e4cac1301012a4
dahyeon-da <pop8261@naver.com>
2024-05-11 02:52:41 +0900

    ✨feat: 스타일 분석이력 스타일 텍스트로 출력하기

--- frontend/mobile_app/lib/src/widget/fl_chart.dart
+++ frontend/mobile_app/lib/src/widget/fl_chart.dart
@@ -28,6 +28,7 @@ class FlChart extends StatelessWidget {
     ];
 
     List<String> stylePercent = [];
+    String favoriteStyle = '';
 
     // styleResult가 null이 아니고 비어있지 않은 경우에만 데이터를 추출합니다.
     if (styleResult != null && styleResult.isNotEmpty) {
@@ -35,9 +36,35 @@ class FlChart extends StatelessWidget {
           styleResult.entries.toList()
             ..sort((a, b) => b.value.compareTo(a.value)));
       // styleResult를 value를 기준으로 내림차순 정렬
+      favoriteStyle = styleResults.keys.first;
+      if (favoriteStyle == "casual")
+        favoriteStyle = "캐주얼";
+      else if (favoriteStyle == "dandy")
+        favoriteStyle = "댄디";
+      else if (favoriteStyle == "street")
+        favoriteStyle = "스트릿";
+      else if (favoriteStyle == "Amekaji")
+        favoriteStyle = "아메카지";
+      else if (favoriteStyle == "gofcore")
+        favoriteStyle = "고프코어";
+      else if (favoriteStyle == "chic")
+        favoriteStyle = "시크";
+      else if (favoriteStyle == "businessCasual")
+        favoriteStyle = "비즈니스캐주얼";
+      else if (favoriteStyle == "Sporty")
+        favoriteStyle = "스포티";
+      else if (favoriteStyle == "golf")
+        favoriteStyle = "골프";
+      else if (favoriteStyle == "minimal")
+        favoriteStyle = "미니멀";
+      else if (favoriteStyle == "romantic")
+        favoriteStyle = "로맨틱";
+      else if (favoriteStyle == "girlish")
+        favoriteStyle = "걸리시";
+      else if (favoriteStyle == "retro") favoriteStyle = "레트로";
 
       if (styleResult.isNotEmpty) {
-        for (int style in styleResult.values) {
+        for (int style in styleResults.values) {
           stylePercent
               .add((style / myStyleLength * 100).toDouble().toStringAsFixed(1));
         }
@@ -57,6 +84,28 @@ class FlChart extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
+          Container(
+            width: double.infinity,
+            margin: EdgeInsets.only(left: 10, top: 15),
+            child: Row(children: [
+              Text(
+                "고객님의 스타일은 ",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                favoriteStyle,
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(255, 191, 25, 1),
+                ),
+              ),
+              Text(
+                "입니다.",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+            ]),
+          ),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.7,
             height: MediaQuery.of(context).size.width * 0.7,
