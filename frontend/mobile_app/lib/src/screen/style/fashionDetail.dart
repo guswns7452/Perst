9eb59c6692a905957786861076091c57bae540ca
dahyeon-da <pop8261@naver.com>
2024-05-12 04:05:16 +0900

    💄: fashionDetail 디자인 수정

--- frontend/mobile_app/lib/src/screen/style/fashionDetail.dart
+++ frontend/mobile_app/lib/src/screen/style/fashionDetail.dart
@@ -19,8 +19,35 @@ class _FashionDetailState extends State<FashionDetail> {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(height: 50),
+          Row(
+            children: [
+              SizedBox(width: 30),
+              Container(
+                child: Text(
+                  "#${widget.fashion.musinsaStyle}",
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 18,
+                      decoration: TextDecoration.underline,
+                      decorationColor:
+                          const Color.fromARGB(255, 255, 255, 255)),
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(3)),
+                padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
+                margin: EdgeInsets.only(right: 2),
+              ),
+              Text(
+                "에 추천되는 코디",
+                style: TextStyle(
+                    fontSize: 23, color: const Color.fromARGB(255, 0, 0, 0)),
+              )
+            ],
+          ),
+          SizedBox(height: 10),
           Center(
             child: Container(
               height: 550,
@@ -29,71 +56,134 @@ class _FashionDetailState extends State<FashionDetail> {
             ),
           ),
           SizedBox(height: 20),
+          Stack(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 60),
+                child: Text(''),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(
+                        widget.fashion.musinsaRed.toInt(),
+                        widget.fashion.musinsaGreen.toInt(),
+                        widget.fashion.musinsaBlue.toInt(),
+                        1),
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.grey, width: 0.3)),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 30),
+                child: _imageBuild(widget.fashion.musinsaFileid),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    border: Border.all(color: Colors.grey, width: 0.3)),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 130),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Model",
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '${widget.fashion.musinsaGender} / ${widget.fashion.musinsaHeight}cm / ${widget.fashion.musinsaWeight}kg',
+                      style: TextStyle(fontSize: 10, color: Colors.grey),
+                    ),
+                    Text(
+                      'Hue / Saturation / Value',
+                      style:
+                          TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '${widget.fashion.musinsaHue} / ${widget.fashion.musinsaSaturation} / ${widget.fashion.musinsaValue}',
+                      style: TextStyle(fontSize: 10, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          SizedBox(height: 20),
           Row(
             children: [
               SizedBox(width: 30),
-              Container(
-                  padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
-                  child: Text(
-                    '#${widget.fashion.musinsaSeason}',
-                    style: TextStyle(
-                        fontSize: 15,
-                        color: Color.fromARGB(255, 127, 127, 127)),
-                  ),
-                  color: Color.fromRGBO(224, 224, 224, 1)),
-              SizedBox(width: 10),
               Container(
                 padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
                 child: Text(
                   '#${widget.fashion.musinsaStyle}',
                   style: TextStyle(
-                      fontSize: 15, color: Color.fromARGB(255, 127, 127, 127)),
+                      fontSize: 13, color: Color.fromARGB(255, 127, 127, 127)),
                 ),
-                color: Color.fromRGBO(224, 224, 224, 1),
+                color: Color.fromRGBO(240, 240, 240, 1),
               ),
+              SizedBox(width: 10),
+              Container(
+                  padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
+                  child: Text(
+                    '#${widget.fashion.musinsaSeason}',
+                    style: TextStyle(
+                        fontSize: 13,
+                        color: Color.fromARGB(255, 127, 127, 127)),
+                  ),
+                  color: Color.fromRGBO(240, 240, 240, 1)),
               SizedBox(width: 10),
               Container(
                   padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
                   child: Text(
                     '#${widget.fashion.musinsaGender}',
                     style: TextStyle(
-                        fontSize: 15,
+                        fontSize: 13,
                         color: Color.fromARGB(255, 127, 127, 127)),
                   ),
-                  color: Color.fromRGBO(224, 224, 224, 1)),
+                  color: Color.fromRGBO(240, 240, 240, 1)),
+              SizedBox(width: 10),
+              Container(
+                  padding: EdgeInsets.fromLTRB(7, 7, 7, 7),
+                  child: Text(
+                    '#${widget.fashion.musinsaPersonal}',
+                    style: TextStyle(
+                        fontSize: 13,
+                        color: Color.fromARGB(255, 127, 127, 127)),
+                  ),
+                  color: Color.fromRGBO(240, 240, 240, 1)),
             ],
           ),
-          SizedBox(height: 10),
-          Row(
-            children: [
-              SizedBox(width: 30),
-              Text(
-                widget.fashion.musinsaGender == "woman" ? "여성" : "남성",
-                style: TextStyle(fontSize: 15),
-              ),
-              Image.asset('assets/dot.png', height: 15),
-              Text(
-                '${widget.fashion.musinsaHeight}cm',
-                style: TextStyle(fontSize: 15),
-              ),
-              Image.asset('assets/dot.png', height: 15),
-              Text(
-                '${widget.fashion.musinsaWeight}kg',
-                style: TextStyle(fontSize: 15),
-              ),
-            ],
-          ),
-          Center(
+          Container(
+            margin: EdgeInsets.only(left: 20),
             child: TextButton(
               onPressed: () {
                 launchUrl(Uri.parse(
                     'https://www.musinsa.com/app/styles/views/${widget.fashion.musinsaNumber}'));
               },
-              child: Text('패션 링크로 이동하기'),
+              child: Text(
+                '해당 코디 자세히 보기 >',
+                style: TextStyle(color: Colors.grey),
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
   }
+}
+
+Widget _imageBuild(String id) {
+  return Container(
+    width: 50,
+    height: 50,
+    decoration: BoxDecoration(
+        image: DecorationImage(
+          alignment: Alignment.topCenter,
+          fit: BoxFit.fitWidth,
+          image:
+              NetworkImage("https://drive.google.com/thumbnail?id=$id&sz=w300"),
+        ),
+        borderRadius: BorderRadius.circular(100)),
+  );
 }
