298b28d56be7d809bf5390515d3963a431fbc5ae
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-05-13 00:45:04 +0900

    🎨 fix: 구조 수정

--- Flask_backend/crawling/main_crawling.py
+++ Flask_backend/crawling/main_crawling.py
@@ -1,7 +1,6 @@
 ## [크롤링] 프로세스 소개
 ## 1. musinsa_brandsnap_crawling 파일 저장
 ## 2. musinsa_codishop_crawling 파일 저장
-## 3. 
 
 from musinsa_brandsnap_crawling import brand_main
 from musinsa_codishop_crawling import codi_main
@@ -21,12 +20,13 @@ codi_save_folderPath = PATH + "\\Image\\newimages\\20240502\\"
 # ------------------------------------------ #
 
 if __name__ == '__main__':
-    # brand_main(brand_save_folderPath)
-    # codi_main(codi_save_folderPath)
+    # 이미지 크롤링
+    brand_main(brand_save_folderPath)
+    codi_main(codi_save_folderPath)
 
 
     ## 퍼스널 컬러 분석
     ## 구글 드라이브 업로드
     ## DB 업로드 
-    # dataChange(brand_save_folderPath, "brandsnap")
+    dataChange(brand_save_folderPath, "brandsnap")
     dataChange(codi_save_folderPath, "codishop")
