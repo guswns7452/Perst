48c78d49e61b771c1ca6385a9a9620d0a45e4e67
전현준 <31847789+guswns7452@users.noreply.github.com>
2024-06-16 21:21:23 +0900

    🐛 fix: DB Delete Transactional

--- backend/src/main/java/com/clothes/perst/persistance/StyleAnalyzeColorRepository.java
+++ backend/src/main/java/com/clothes/perst/persistance/StyleAnalyzeColorRepository.java
@@ -1,6 +1,7 @@
 package com.clothes.perst.persistance;
 
 import com.clothes.perst.domain.StyleColorVO;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ public interface StyleAnalyzeColorRepository extends JpaRepository<StyleColorVO,
     StyleColorVO save(StyleColorVO styleAnalyzeColor);
 
     List<StyleColorVO> findAllByStyleNumber(int styleNumber);
+    @Transactional
     void deleteByStyleNumber(int styleNumber);
 }
--- backend/src/main/java/com/clothes/perst/persistance/StyleAnalyzeRepository.java
+++ backend/src/main/java/com/clothes/perst/persistance/StyleAnalyzeRepository.java
@@ -1,6 +1,7 @@
 package com.clothes.perst.persistance;
 
 import com.clothes.perst.domain.StyleAnalyzeVO;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +15,6 @@ public interface StyleAnalyzeRepository extends JpaRepository<StyleAnalyzeVO, St
 
     List<StyleAnalyzeVO> findAllByMemberNumber(int memberNumber);
 
+    @Transactional
     void deleteByStyleNumber(int styleNumber);
 }
